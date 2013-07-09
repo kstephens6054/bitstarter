698721db549ff635bddd6b3b5774b9b49b26888f
Kurt Stephens <kstephens6054@gmail.com>
2013-07-09 03:35:02 +0000

    Added index.html handler to web.js.

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
@@ -8,6 +9,14 @@ app.get('/', function(request, response) {
 
 app.get('/index.html', function(request, response) {
   response.send('Sending index.html');
+  fs.readFile('./index.html', function(error, data) {
+    if (!error) {
+      response.send(data);
+    }
+    else {
+      response.send(error);
+    }
+  };
 });
 
 var port = process.env.PORT || 5000;
