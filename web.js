e52a9c4c36a93ad735a39d58de9b2c778dfa992b
Kurt Stephens <kstephens6054@gmail.com>
2013-07-09 03:27:50 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -6,6 +6,10 @@ app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
 
+app.get('/index.html', function(request, response) {
+  response.send('Sending index.html');
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
